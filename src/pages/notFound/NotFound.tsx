86b39a19856f45b14f525345d22d4f35a05c73bd
Sebastian G <seba.ri@mail.ru>
2025-06-16 17:20:40 +0300

    typed NotFound

---
+++ src/pages/notFound/NotFound.tsx
@@ -0,0 +1,36 @@
+// import { Link } from 'react-router-dom';
+// import './NotFound.scss';
+
+// const NotFound = () => {
+//   return (
+//     <div className="not-found">
+//       <h1 className="not-found__title">404 - Pagina nu a fost găsită</h1>
+//       <p className="not-found__text">Ups! Pagina pe care o cauți nu există.</p>
+//       <Link to="/" className="not-found__link">
+//         ← Înapoi la Acasă
+//       </Link>
+//     </div>
+//   );
+// };
+
+// export default NotFound;
+
+
+// typescript + react
+
+import { Link } from 'react-router-dom';
+import './NotFound.scss';
+
+const NotFound: React.FC = () => {
+  return (
+    <div className="not-found">
+      <h1 className="not-found__title">404 - Page not found</h1>
+      <p className="not-found__text">Oops! The page you are looking for does not exist.</p>
+      <Link to="/" className="not-found__link">
+        ← Back to Home
+      </Link>
+    </div>
+  );
+};
+
+export default NotFound;
